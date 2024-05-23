257a46ec2e258de6b9b8bea0420f4c36b7bc0058
SawNeon <maheusbastost313@gmail.com>
2024-05-23 12:01:27 -0300

    Very bad solution

---
+++ course/src/entities/Reservation.java
@@ -0,0 +1,67 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); 
+	
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+	
+	public Reservation() {
+	}
+
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckin() {
+		return checkIn;
+	}
+
+	public Date getCheckout() {
+		return checkOut;
+	}
+
+	
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		s.append("Room ");
+		s.append(roomNumber);
+		s.append(", check-in: ");
+		s.append(sdf.format(checkIn));
+		s.append(", check-out: ");
+		s.append(sdf.format(checkOut));
+		s.append(", ");
+		s.append(duration());
+		s.append(" nights");
+		
+		
+		return s.toString();
+	}
+}
